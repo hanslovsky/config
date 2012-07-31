b4410831d78220fa1e11274ac850ff1a3c9f5caf
Philipp Hanslovsky <Philipp.Hanslovsky@gmail.com>
2012-07-31 18:02:45 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 repoDir=$(pwd)
 cd $HOME
 echo "removing existing config files"
-rm -rfv .bash .bashrc .conky .emacs .emacs.d .emacs-themes
+rm -rfv .bash .bashrc .conky .emacs .emacs-themes
 
 echo "linking new config files"
 cd $repoDir
@@ -13,5 +13,5 @@ ln -s $repoDir/.bash $HOME/.bash
 ln -s $repoDir/.bashrc $HOME/.bashrc
 ln -s $repoDir/.conky $HOME/.conky
 ln -s $repoDir/.emacs $HOME/.emacs
-ln -s $repoDir/.emacs.d $HOME/.emacs.d
+# ln -s $repoDir/.emacs.d $HOME/.emacs.d
 ln -s $repoDir/.emacs-themes/ $HOME/.emacs-themes
