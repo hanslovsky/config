22c36ab53b71b934c75147eceb6a9e4428ca33a8
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-09-08 21:59:41 +0200

    disabled some sublimity stuff

--- .emacs.d/config/sublimity-conf.el
+++ .emacs.d/config/sublimity-conf.el
@@ -7,18 +7,18 @@
 ;; make sublimity available everywhere
 (sublimity-global-mode)
 
-;; update map on the following commands
-(setq sublimity-map-on-commands
-      '(previous-line next-line backward-paragraph forward-paragraph
-                      end-of-defun beginning-of-defun))
+;; update map on the following commands 
+;; (setq sublimity-map-on-commands
+;;       '(previous-line next-line backward-paragraph forward-paragraph
+;;                       end-of-defun beginning-of-defun))
 
 ;; sublimity size
 (setq sublimity-map-size 30)
 (setq sublimity-map-fraction 0.3)
 
-;; make scrolling smooth
-(setq sublimity-scroll-weight 5
-      sublimity-scroll-drift-length 10)
+;; make scrolling smooth - does not feel that good
+;; (setq sublimity-scroll-weight 5
+;;      sublimity-scroll-drift-length 10)
 
 ;; no timer for sublimity
 (sublimity-map-set-delay 2)
