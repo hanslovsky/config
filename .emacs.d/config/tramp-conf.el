85b2c0412ec2f43290891aee680263adcf646716
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2013-11-16 11:13:36 +0100

    tramp: sudo & ssh

--- .emacs.d/config/tramp-conf.el
+++ .emacs.d/config/tramp-conf.el
@@ -1,2 +1,6 @@
 ;; tramp
 (require 'tramp)
+
+;; use sudo and ssh in conjunction: C-x C-f /sudo:root@host[#port]:/path/to/file
+;; see also: http://www.gnu.org/software/emacs/manual/html_node/tramp/Multi_002dhops.html
+;(set-default 'tramp-default-proxies-alist (quote ((nil "\\`root\\'" "/ssh:%h:"))))
