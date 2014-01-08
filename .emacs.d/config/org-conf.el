5670031e5a92496bcbaaad49905ea0cd2119540c
Philipp Hanslovsky <Philipp.Hanslovsky@iwr.uni-heidelberg.de>
2014-01-08 14:28:31 +0100

    added tabularx and tabu support for org

--- .emacs.d/config/org-conf.el
+++ .emacs.d/config/org-conf.el
@@ -5,3 +5,9 @@
 (define-key global-map "\C-ca" 'org-agenda)
 (setq org-log-done t)
 (setq org-agenda-files (list "~/.emacs.d/org/basics.org"))
+(when (not (boundp 'org-latex-packages-alist))
+  (setq org-latex-packages-alist '())
+  )
+(add-to-list 'org-latex-packages-alist 'tabularx)
+(add-to-list 'org-latex-packages-alist 'tabu)
+
