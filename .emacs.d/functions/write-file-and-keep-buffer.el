410fac82da6271d12093ee0e602d69120cb5ee5e
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-06-02 09:31:05 -0400

    write file and keep buffer

---
+++ .emacs.d/functions/write-file-and-keep-buffer.el
@@ -0,0 +1,10 @@
+;; taken from http://stackoverflow.com/a/5168886/1725687
+
+
+(defun write-file-and-keep-buffer (filename)
+  "Write the current buffer into FILENAME and keep original buffer"
+  (interactive "Write to file: ")
+  (save-restriction
+    (widen)
+    (write-region (point-min) (point-max) filename nil nil nil 'confirm))
+  (find-file filename))
