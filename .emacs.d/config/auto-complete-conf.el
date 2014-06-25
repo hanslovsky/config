901672e19bb4e7c4b62832e5e5e674fb2292bb1c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-06-24 23:46:20 -0400

    added auto-complete for c and c++ headers

--- .emacs.d/config/auto-complete-conf.el
+++ .emacs.d/config/auto-complete-conf.el
@@ -4,6 +4,8 @@
 
 
 (install_if_missing 'auto-complete)
+(install_if_missing 'auto-complete-c-headers)
+(install_if_missing 'auto-complete-auctex)
 
 (when (not (boundp 'elpa-packages-list))
   (setq elpa-packages-list '())
@@ -16,3 +18,31 @@
 (setq ac-comphist-file (expand-file-name
                         "~/.emacs.d/ac-comphist.dat"))
 (ac-config-default)
+
+;; define include search patterns
+(setq achead:include-patterns '("\\.\\(h\\|hpp\\|hh\\|hxx\\)$" "/[a-zA-Z-_]+$") )
+
+
+(defun my:ac-c-header-init()
+  (require 'auto-complete-c-headers)
+  (add-to-list 'ac-sources 'ac-source-c-headers)
+  
+  ;; add paths from environment
+  (setq my:c++-headers (split-string (getenv "CPLUS_INCLUDE_PATH") ":" t ) )
+  (setq my:c-headers (split-string (getenv "C_INCLUDE_PATH") ":" t ) )
+
+  (dolist (element my:c++-headers)
+    (when (file-exists-p element)
+      (add-to-list 'achead:include-directories element)
+      )
+    )
+  (dolist (element my:c-headers)
+    (when (file-exists-p element)
+      (add-to-list 'achead:include-directories element)
+      )
+    )
+  )
+
+(add-hook 'c++-mode-hook 'my:ac-c-header-init)
+(add-hook 'c-mode-hook 'my:ac-c-header-init)
+
