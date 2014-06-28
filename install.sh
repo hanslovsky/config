0dbbba8adf857aaedd76d5eb2cfe6ce584dd38f6
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-06-28 10:39:43 -0400

    moved all dotfiles to dotfiles directory and simplified install script

--- install.sh
+++ install.sh
@@ -1,76 +1,47 @@
 #!/bin/bash
 
-set -e
+# set -e
 
-repoDir=$(pwd)
-targets=(".sh"
-    ".bashrc"
-    ".zshrc"
-    ".conky"
-    ".emacs"
-    ".emacs-themes"
-    ".fonts"
-    ".gitconfig"
-    ".gitignore"
-    ".i3"
-    ".xsessionrc"
-    ".tmux.conf"
-    ".offlineimaprc"
-    "$(ls .emacs.d | while read x; do echo .emacs.d/$x; done)"
-    "$(ls .ipython | while read x; do echo .ipython/$x; done)"
-    ".latexmkrc"
-    "$(ls texmf/tex/latex | while read x; do echo texmf/tex/latex/$x; done)"
-    ".xinitrc")
-cd $HOME
-
-if ! [[ -d ".emacs.d" ]]; then
-    echo "Need to create ~/.emacs.d"
-    mkdir -p .emacs.d
-fi
-
-if ! [[ -d ".ipython" ]]; then
-    echo "Need to create ~/.ipython"
-    mkdir -p .ipython
-fi
-
-if ! [[ -d "~/texmf/tex/latex" ]]; then
-    echo "Need to create ~/texmf/tex/latex"
-    mkdir -p texmf/tex/latex
-fi
-
-echo "removing existing config files..."
-for t in ${targets[@]}; do
-    rm -rf $t
-    echo "  $t"
-done
+OLD_CWD="$PWD"
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+BACKUP_SUFFIX="${BACKUP_SUFFIX:-}"
+NO_ZSH="${NO_ZSH:-}"
 
 
-echo "linking new config files"
-cd $repoDir
-for t in ${targets[@]}; do
-    ln -s "$repoDir/$t" "$HOME/$t"
-    echo "  $HOME/$t -> $repoDir/$t"
-done
+cd "$ROOT_DIR/dotfiles"
 
-# check for oh-my-zsh
-if [[ -d "$HOME/git/oh-my-zsh" ]]; then
-    if [[ -e "$HOME/.oh-my-zsh" ]]; then
-	echo "~/.oh-my-zsh already exists"
-    else
-	echo "linking oh-my-zsh"
-	ln -s "$HOME/git/oh-my-zsh" "$HOME/.oh-my-zsh"
-    fi
+# create directories if neccessary
+find . ! -path . -type d -exec mkdir -p "$HOME/"{} \;
+
+# rm existing config files and link to those in the repository
+if [ -z "$BACKUP_SUFFIX" ]; then
+    find . -type f -exec rm -f "$HOME/"{} \; -exec ln -s "$PWD/"{} "$HOME/"{} \;
 else
-    echo "$HOME/git/oh-my-zsh does not exist"
-    echo "  type \"git clone git://github.com/robbyrussell/oh-my-zsh.git\" to get it"
+    find . -type f -exec mv -n "$HOME/"{} "$HOME/"{}"$BACKUP_SUFFIX" \; -exec ln -s "$PWD/"{} "$HOME/"{} \;
 fi
 
-# set standard shell
-std_shell="$(which zsh)"
-if [ -n "$std_shell" ]; then
-    if [ -z "$(cat /etc/passwd | grep $USER | grep zsh)" ]; then
-        echo "setting standard shell to $std_shell"
-        chsh -s $std_shell
+cd "$OLD_CWD"
+
+if [ -z "$NO_ZSH" ]; then
+    # check for oh-my-zsh
+    if [[ -d "$HOME/git/oh-my-zsh" ]]; then
+        if [[ -e "$HOME/.oh-my-zsh" ]]; then
+	    echo "~/.oh-my-zsh already exists"
+        else
+	    echo "linking oh-my-zsh"
+	    ln -s "$HOME/git/oh-my-zsh" "$HOME/.oh-my-zsh"
+        fi
+    else
+        echo "$HOME/git/oh-my-zsh does not exist"
+        echo "  type \"git clone git://github.com/robbyrussell/oh-my-zsh.git\" to get it"
+    fi
+
+    # set standard shell
+    std_shell="$(which zsh)"
+    if [ -n "$std_shell" ]; then
+        if [ -z "$(cat /etc/passwd | grep $USER | grep zsh)" ]; then
+            echo "setting standard shell to $std_shell"
+            chsh -s $std_shell
+        fi
     fi
 fi
-
