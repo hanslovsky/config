e5f83ec3c602457967dc1ece4faf7cc99c134a57
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-01-16 13:49:18 -0500

    added find-extensions to show all files with given extensions within directory

--- dotfiles/.sh/sh_convenience_functions.sh
+++ dotfiles/.sh/sh_convenience_functions.sh
@@ -1,3 +1,19 @@
 function show-only-common {
     comm -12 <( sort $1 )  <( sort $2 )
 }
+
+function find-extensions {
+    USAGE="$0 <dir> <extension> [<extension> ...]"
+    if [[ "$#" -lt "2" ]]; then
+        echo "Need at least two arguments!"
+        echo "$USAGE"
+        return 1
+    fi
+    ADDITIONAL_ARGS="${ADDITIONAL_ARGS:-}"
+    directory=$1
+    shift
+    extensions=$(echo "$@" | sed 's/ /\|/g')
+    COMMAND="find $directory -type f -regextype posix-extended -regex \".*\.($extensions)\" $ADDITIONAL_ARGS"
+    sh -c "$COMMAND"
+    return 0
+}
