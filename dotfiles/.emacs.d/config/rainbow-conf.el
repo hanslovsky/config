d7c46bce45d977788035845d4c0068cf49cce7d0
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-06-17 10:33:09 -0400

    more colors!

--- dotfiles/.emacs.d/config/rainbow-conf.el
+++ dotfiles/.emacs.d/config/rainbow-conf.el
@@ -1,5 +1,13 @@
 (install_if_missing 'rainbow-mode)
+(install_if_missing 'rainbow-delimiters)
+(install_if_missing 'rainbow-identifiers)
+(install_if_missing 'rainbow-blocks)
 (define-globalized-minor-mode my-global-rainbow-mode rainbow-mode
   (lambda () (rainbow-mode 1)))
 
 (my-global-rainbow-mode 1)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-identifiers-mode)
+
+;; make own conf file for lisp?
+(add-hook 'prog-mode-hook #'rainbow-blocks-mode)
