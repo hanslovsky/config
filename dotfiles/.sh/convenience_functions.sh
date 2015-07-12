31cd983c0342058c639b601b4040b1fc0a2c7b7c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-07-12 19:02:36 -0400

    interactively modify PATH variables

--- dotfiles/.sh/convenience_functions.sh
+++ dotfiles/.sh/convenience_functions.sh
@@ -24,3 +24,66 @@ function find-extensions {
 function envof {
     sed 's/\x0/\n/g' /proc/${1}/environ;
 }
+
+
+# pick line and modify
+function pick_line_and_modify
+{
+    LTE='\xe2\x89\xa4'
+    VAL=$(cat $1)
+    ENUM=$(nl <(echo $VAL))
+    echo $ENUM >&2
+    MAX_NUM=`echo -n $ENUM | tail -n1 | sed -r 's/^[[:space:]]+//' | sed -r 's/[[:space:]]+.*$//'`
+    LINE=0
+    while [ $LINE -lt 1 -o $LINE -gt $MAX_NUM ]; do
+        echo -e "Pick 1 $LTE number $LTE $MAX_NUM to modify line" >&2
+        read LINE
+        echo >&2
+        LINE=`echo $LINE | sed 's/[^0-9]+//g'`
+    done
+    CHOICE=''
+    while ; do
+        echo 'Modify(m)/Delete(d)/No-Action(n)/Quit(q)?' >&2
+        read CHOICE
+        echo >&2
+        CHOICE=`echo -n $CHOICE | sed 's/[^dmnqDMNQ]//g' | tr '[:upper:]' '[:lower:]'`
+        if [ ! $CHOICE = "" ]; then
+            if [ $CHOICE = d -o $CHOICE = m -o $CHOICE = n -o $CHOICE = q ]; then
+                break
+            fi
+        fi
+    done
+    case "$CHOICE" in
+        m)
+            CONTENT=`echo $VAL | sed -n "${LINE}p"`
+            echo "Please specify replacement for line $LINE: $CONTENT" >&2
+            read REPLACEMENT
+            echo >&2
+            echo -n $VAL | sed "${LINE}s#^.*#$REPLACEMENT#"
+            ;;
+        d)
+            echo -e $VAL | sed -e "${LINE}d"
+            ;;
+        n)
+            echo $VAL
+            ;;
+        q)
+            echo $VAL
+            ;;
+        *)
+            echo $VAL
+            ;;
+    esac
+}
+
+
+function modify_line_in_path
+{
+    PATH_VAR_NAME=$1
+    PATH_VAR=`eval echo $"$PATH_VAR_NAME"`
+    NL_PATH_VAR="${PATH_VAR//:/
+}"
+    MODIFIED=$(pick_line_and_modify <(echo -n $NL_PATH_VAR) )
+    export $PATH_VAR_NAME=$(echo -n $MODIFIED | tr '\n' ':' )
+    PATH_VAR=`eval echo $"$PATH_VAR_NAME"`
+}
