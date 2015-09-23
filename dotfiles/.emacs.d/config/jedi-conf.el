b1e79163d84fd88a18bafa5fa1b967813e9833dc
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-09-23 02:43:13 -0400

    company-jedi

--- dotfiles/.emacs.d/config/jedi-conf.el
+++ dotfiles/.emacs.d/config/jedi-conf.el
@@ -1,2 +1,8 @@
 (install_if_missing 'jedi)
+(install_if_missing 'company-jedi)
+
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
 
