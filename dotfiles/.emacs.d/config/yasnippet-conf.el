2d781e60de27ff01d8caa0f4a5a80be5d8e067e2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-01-15 15:32:21 -0500

    updated yasnippet config

--- dotfiles/.emacs.d/config/yasnippet-conf.el
+++ dotfiles/.emacs.d/config/yasnippet-conf.el
@@ -1,8 +1,11 @@
+(install_if_missing 'yasnippet)
 (install_if_missing 'auto-yasnippet)
 
+
+
+;; (require 'yasnippet)
+;; (require 'auto-yasnippet)
+
+(add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippets/") 
 (yas-global-mode 1)
-
-(require 'yasnippet)
-(require 'auto-yasnippet)
-
 
