df5d05214ff880ce40137d365601fbd47f5ac2bb
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-01-16 12:33:33 -0500

    added yasnippet to add license header

--- dotfiles/.emacs.d/config/yasnippet-conf.el
+++ dotfiles/.emacs.d/config/yasnippet-conf.el
@@ -1,10 +1,30 @@
 (install_if_missing 'yasnippet)
 (install_if_missing 'auto-yasnippet)
 
+(defun my:insert-and-comment-file-contents (path)
+  (let
+      (
+       (my:major-mode (buffer-local-value 'major-mode (current-buffer)))
+       (my:comment-start comment-start)
+       (my:comment-end comment-end)
+       )
+    (message "%s" my:major-mode)
+    (with-temp-buffer
+      ;; (funcall my:major-mode)
+      (insert-file-contents path)
+      (let
+          (
+           (comment-start my:comment-start)
+           (comment-end my:comment-end)
+           )
+        (comment-region (point-min) (point-max))
+        (buffer-substring-no-properties (point-min) (point-max))
+        )
+      )
+    )
+  )
 
-
-;; (require 'yasnippet)
-;; (require 'auto-yasnippet)
+(setq my:licenses-path "~/.licenses")
 
 (add-to-list 'yas-snippet-dirs "~/.emacs.d/yasnippets/") 
 (yas-global-mode 1)
