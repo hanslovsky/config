4b200361146aeb68c7abb3a1915cb85848d8c5d5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-01-24 21:23:04 -0500

    added function to find occurance of filename as close to root as possible

--- dotfiles/.emacs.d/functions/utility.el
+++ dotfiles/.emacs.d/functions/utility.el
@@ -36,4 +36,25 @@
 (global-set-key (kbd "C-c M-f") 'copy-filename-at-point)
 (global-set-key (kbd "C-c M-s") 'copy-sentence-at-point)
 
+(defun find-deepest-occurrence (path filename)
+  (interactive)
+  (let (
+        (components (reverse (split-string path "/" t)))
+        (my:result nil)
+        )
+    (when (equal (substring path 0 1) "/")
+      (setq components (append components '("")))
+      )
+      (while components
+        (let ((current-dir (string-join (reverse components) "/"))) ;; better way to do this?
+          (when (equal current-dir "")
+            (setq current-dir "/"))
+          (when (file-exists-p (concat (file-name-as-directory current-dir) filename))
+                (setq my:result current-dir)
+                )
+          )
+        (setq-local components (cdr components)))
+      (identity my:result)
+      )
+  )
 
