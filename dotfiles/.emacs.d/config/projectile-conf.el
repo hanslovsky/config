2e7dd93b3a13519dc02a93a0c7c61d555fd7ecd5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-08-17 14:21:30 -0400

    Use persp-projectile.

--- dotfiles/.emacs.d/config/projectile-conf.el
+++ dotfiles/.emacs.d/config/projectile-conf.el
@@ -1,6 +1,7 @@
 (install_if_missing 'projectile)
 (install_if_missing 'helm-projectile)
 (install_if_missing 'org-projectile)
+(install_if_missing 'persp-projectile)
 
 (projectile-global-mode)
 (setq projectile-enable-caching t)
