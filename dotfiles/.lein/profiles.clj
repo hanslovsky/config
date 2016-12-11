a0d9b5c8aab291139761fbc0ba8efe2fb0edad25
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-12-11 11:27:37 +0100

    Add plugins to lein

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,2 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]]}}
-;;{:user {:plugins [[venantius/ultra "0.5.0"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
+                  [venantius/ultra "0.5.0"]
+                  [lein-webrepl "0.1.0-SNAPSHOT"]
+                  ]}}
