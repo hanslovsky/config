af2431088962cf58d50afa6a5bae7a33dae538cc
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-12-11 17:30:51 +0100

    load cider nrepl only in repl profile

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -1,4 +1,6 @@
-{:user {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
-                  [venantius/ultra "0.5.0"]
+{:user {:plugins [[venantius/ultra "0.5.0"]
                   [lein-webrepl "0.1.0-SNAPSHOT"]
+                  ]}}
+
+{:repl {:plugins [[cider/cider-nrepl "0.15.0-SNAPSHOT"]
                   ]}}
