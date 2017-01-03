a4ec2129c1267e4f6b9c0185d4f976808be3fe28
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2017-01-03 12:29:50 -0500

    Exception safe import in ipython config

--- dotfiles/.ipython/profile_default/startup/00-imports.py
+++ dotfiles/.ipython/profile_default/startup/00-imports.py
@@ -1,5 +1,20 @@
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import numpy as np
-import seaborn as sns
-import vigra
+from __future__ import division, print_function
+
+import importlib
+import traceback
+
+
+modules = {
+	'matplotlib' : 'mpl',
+	'matplotlib.pyplot' : 'plt',
+	'numpy' : 'np',
+	'seaborn' : 'sns',
+	'vigra' : ''
+	}
+
+for k,v in modules.items():
+	try:
+		globals()[k if v == '' else v]  = importlib.import_module(k)
+	except ImportError as e:
+		print("Could not load module " + k)
+		traceback.print_exc()
