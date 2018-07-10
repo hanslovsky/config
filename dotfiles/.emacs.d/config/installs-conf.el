e03a72d822b50e2bbe49d8d200f55243106cde14
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-07-10 11:09:13 -0400

    Do not load/run broken packages/functions

--- dotfiles/.emacs.d/config/installs-conf.el
+++ dotfiles/.emacs.d/config/installs-conf.el
@@ -1,6 +1,6 @@
 ;; loose collection of things to install that do not require config; create own file once a package requires installation
 
-(install_if_missing 'vkill)
+;; (install_if_missing 'vkill)
 
 (install_if_missing 'vline)
 
