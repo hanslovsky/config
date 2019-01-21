653cfd0358b496f1646826496fc9aebaa728d443
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-21 10:06:17 -0500

    Migrate methods from separate file into elpa config

--- dotfiles/.emacs.d/config/elpa-conf.el
+++ dotfiles/.emacs.d/config/elpa-conf.el
@@ -4,13 +4,24 @@
                          ("melpa-stable" . "https://stable.melpa.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")))
 
-(when (not (boundp 'package--old-archive-priorities))
-  (setq package-archive-priorities
-        '(("melpa-stable" . 40)
-          ("melpa" . 35)
-          ("marmalade" . 20)
-          ("gnu" . 10)))
+(defvar package-archive-priorities
+      '(("melpa-stable" . 40)
+        ("melpa" . 35)
+        ("marmalade" . 20)
+        ("gnu" . 10)))
+
+(defun install_if_missing (package_name)
+  (when (not (package-installed-p package_name))
+    (update_packages_if_neccessary)
+    (message "Installing package %s..." package_name)
+    (package-install package_name)
+    (message "Installing package %s... done" package_name)
+    )
   )
 
-;; install packages automatically
-(load-file "~/.emacs.d/functions/acquire_packages_if_neccessary.el")
+(defun update_packages_if_neccessary ()
+  (when (not (boundp 'elpa_update_check))
+    (setq elpa_update_check t)
+    (package-refresh-contents)
+    )
+  )
