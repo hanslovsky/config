0753fa075676fd1155d54df4bdca7c3ae18facb5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-21 09:43:46 -0500

    Remove vline mode
    Emacs becomes super slow with it

--- dotfiles/.emacs.d/config/installs-conf.el
+++ dotfiles/.emacs.d/config/installs-conf.el
@@ -1,7 +1,4 @@
 ;; loose collection of things to install that do not require config; create own file once a package requires installation
 
-;; (install_if_missing 'vkill)
-(install_if_missing 'vline)
-(vline-global-mode)
 (install_if_missing 'volatile-highlights)
 (install_if_missing 'use-package)
