75ec703520e2331fc2e491af067714a9603b76ec
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-21 12:01:17 -0500

    Remove marmalade repo

--- dotfiles/.emacs.d/config/elpa-conf.el
+++ dotfiles/.emacs.d/config/elpa-conf.el
@@ -1,8 +1,7 @@
 ;; set repositories for elpa
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("melpa" . "http://melpa.org/packages/")
-                         ("melpa-stable" . "https://stable.melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
+                         ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (defvar package-archive-priorities
       '(("gnu" . 30)
