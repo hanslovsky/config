4a38c09b0858c25d368436346658df3d08adc4d2
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2019-01-22 14:19:25 -0500

    Use use-package for markdown mode
    much simpler and concise

--- dotfiles/.emacs.d/config/markdown.el
+++ dotfiles/.emacs.d/config/markdown.el
@@ -1,27 +1,14 @@
-(defvar my/markdown-preview nil)
-(defun my/update-markdown-preview()
-  (when my/markdown-preview (funcall my/markdown-preview)))
+(use-package
+  markdown-mode
+  :defer t
+  :ensure t
+  :mode (("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode)
+         ("README\\.md\\'" . gfm-mode)))
 
-(defun my/initialize-markdown (mode)
-  (when (not (boundp 'my/markdown-initialized))
-    (defvar my/markdown-initialized t)
-    ;; (setq my/markdown-preview 'vmd-mode)
-    (setq my/markdown-preview 'markdown-preview-mode)
-    (install_if_missing 'vmd-mode)
-    (install_if_missing 'markdown-mode)
-    (install_if_missing 'markdown-mode+)
-    (install_if_missing 'markdown-preview-mode)
-    (add-hook 'markdown-mode-hook 'my/update-markdown-preview))
-  (funcall mode))
-
-(defun my/initialize-markdown-markdown-mode ()
-  (my/initialize-markdown 'markdown-mode))
-
-(defun my/initialize-markdown-gfm-mode ()
-  (my/initialize-markdown 'gfm-mode))
-
-
-
-(add-to-list 'auto-mode-alist '("\\.md\\'" . my/initialize-markdown-markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . my/initialize-markdown-markdown-mode))
-(add-to-list 'auto-mode-alist '("README\\.md\\'" . my/initialize-markdown-gfm-mode))
+(use-package
+  vmd-mode
+  :defer t
+  :after markdown-mode
+  :ensure t
+  :bind (:map markdown-mode-map ("<f9>" . vmd-mode)))
