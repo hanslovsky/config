e4a2f891ef6ced140c7f6a042895f1432907525f
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2023-05-09 14:53:49 -0400

    Update color themes config

--- dotfiles/.emacs.d/config/themes-conf.el
+++ dotfiles/.emacs.d/config/themes-conf.el
@@ -1,30 +1,27 @@
-(install_if_missing 'theme-changer)
-(install_if_missing 'gandalf-theme)
-(install_if_missing 'zenburn-theme)
-(install_if_missing 'railscasts-theme)
-;; (install_if_missing 'hipster-theme) not available currently
-(install_if_missing 'powerline) ;; https://github.com/milkypostman/powerline ;; not sure if wanna use it
-(install_if_missing 'moe-theme) ;; https://github.com/kuanyui/moe-theme.el
-(install_if_missing 'material-theme) ;; pretty good theme
-(install_if_missing 'minimal-theme)
-(install_if_missing 'abyss-theme)
-(install_if_missing 'seti-theme) ;; pretty good dark theme
-(install_if_missing 'ample-theme) ;; dark theme (flat version is pretty good, light might lack contrast)
-(install_if_missing 'monokai-theme) ;; dark theme, seems to be sublime inspired
-(install_if_missing 'color-theme-sanityinc-tomorrow) ;; eighties, night seem good, day is a good light theme
-(install_if_missing 'alect-themes) ;; alect-dark-alt seems to be nice dark theme
-(install_if_missing 'hemisu-theme) ;; try these for now
-(install_if_missing 'waher-theme ) ;; too much contrast!
-(install_if_missing 'solarized-theme)
+(use-package theme-changer :ensure t)
+(use-package gandalf-theme :ensure t)
+(use-package zenburn-theme :ensure t)
+(use-package railscasts-theme :ensure t)
+;; (use-package moe-theme :ensure t) ;; https://github.com/kuanyui/moe-theme.el ;; currently not working for some reason
+(use-package material-theme :ensure t) ;; pretty good theme
+(use-package minimal-theme :ensure t)
+(use-package abyss-theme :ensure t)
+(use-package seti-theme :ensure t) ;; pretty good dark theme
+(use-package ample-theme :ensure t) ;; dark theme (flat version is pretty good, light might lack contrast)
+(use-package monokai-theme :ensure t) ;; dark theme, seems to be sublime inspired
+(use-package color-theme-sanityinc-tomorrow :ensure t) ;; eighties, night seem good, day is a good light theme
+(use-package alect-themes :ensure t) ;; alect-dark-alt seems to be nice dark theme
+(use-package hemisu-theme :ensure t) ;; try these for now
+(use-package waher-theme :ensure t ) ;; too much contrast!
+(use-package solarized-theme :ensure t)
 
-(require 'powerline) ;; needs to come before moe-theme
-(require 'moe-theme)
+;; Show highlighted buffer-id as decoration. (Default: nil)
+;; (setq moe-theme-highlight-buffer-id t)
+;; (setq moe-theme-resize-org-title nil) ;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
+
+
+;; config for theme changer
 (require 'json)
- ;; Show highlighted buffer-id as decoration. (Default: nil)
-(setq moe-theme-highlight-buffer-id t)
-(setq moe-theme-resize-org-title nil) ;; (setq moe-theme-resize-org-title '(1.5 1.4 1.3 1.2 1.1 1.0 1.0 1.0 1.0))
-(moe-light)
-
 (defvar my:location-info '((countryCode . "US")
                            (city . "Alexandria")
                            (region . "VA")
@@ -46,13 +43,5 @@
   (setq my:location-info (json-read-from-string (shell-command-to-string '"curl -s 'http://ip-api.com/json'" )))
   (my:update-calendar-from-location-info))
 
-;; theme changer not working as of 2018-07-10
-(require 'theme-changer)
-;; (change-theme 'moe-light 'seti)
-;; (change-theme 'hemisu-light 'seti)
-;; good themes: gandalf (light), (n)zenburn(dark), adawaita (light), tango(-dark), wombat (dark), tsdh-light(light)
-;; good themes: moe-{light,dark}
-;; good themes: material{,-light}
-;; (load-theme 'moe-light t)
-(change-theme 'solarized-light 'material)
 
+(change-theme 'tsdh-light 'alect-dark)
