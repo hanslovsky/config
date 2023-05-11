8a9790e33a51c1272b9db4a5bb59880d1eb06421
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2023-05-11 09:16:00 -0400

    Fix rainbow-delimiters config

--- dotfiles/.emacs.d/config/rainbow-conf.el
+++ dotfiles/.emacs.d/config/rainbow-conf.el
@@ -5,6 +5,6 @@
     (lambda () (rainbow-mode 1)))
   (global-rainbow-mode 1) ;; enable rainbow mode in every buffer (global minor mode)
   )
-(use-package rainbow-delimiters :ensure t :hook (prog-mode LaTeX-mode))
+(use-package rainbow-delimiters :ensure t :hook (prog-mode . rainbow-delimiters-mode))
 (use-package rainbow-identifiers :ensure t)
 
